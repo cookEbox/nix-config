d0be40b939f64cdf46d07174d2a75fe1b785675c
Nick Cooke <nj.cooke@outlook.com>
2023-09-14 17:57:33 +0100

    more hoogle for telescope

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -13,6 +13,7 @@ require('telescope').setup {
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
+require('telescope').load_extension('hoogle')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
