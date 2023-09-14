7122b99117c3f64d8ca77d6e301ce301a8013caa
Nick Cooke <nj.cooke@outlook.com>
2023-09-14 18:10:13 +0100

    hoogle shortcuts

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -13,7 +13,7 @@ require('telescope').setup {
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
-
+require('telescope').load_extension('hoogle')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -31,6 +31,6 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>hh', require('telescope').load_extension('hoogle'), {desc = 'Searc[h] [H]oogle'})
+vim.keymap.set('n', '<leader>hh', "Telscope hoogle")
 
 
