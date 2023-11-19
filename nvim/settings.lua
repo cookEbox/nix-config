fb52f0bd2c5809986a14728a7d29b117f5bd9f22
Nick Cooke <nj.cooke@outlook.com>
2023-11-19 18:19:25 +0000

    use mode not redraw! to redraw buffer

--- nvim/settings.lua
+++ nvim/settings.lua
@@ -22,18 +22,18 @@ vim.cmd([[command! -nargs=0 Wb lua RunWb()]])
 function RunWb()
   vim.cmd('wa')
   _G.runScript()
-  vim.cmd('redraw!')
+  vim.cmd('mode')
 end
 
 function _G.runScript()
     local script_path = './bsync.sh'
     local cmd = 'sh ' .. script_path
-    local success, exit_code, _ = os.execute(cmd)
-    if not success or exit_code ~= 0 then
-        vim.api.nvim_err_writeln('Error running script: ' .. script_path)
-    else
-        vim.api.nvim_out_write('Script executed successfully\n')
-    end
+    os.execute(cmd)
+    -- if not success or exit_code ~= 0 then
+    --     vim.api.nvim_err_writeln('Error running script: ' .. script_path)
+    -- else
+    --     vim.api.nvim_out_write('Script executed successfully\n')
+    -- end
 end
 
 -- [[ Setting options ]]
