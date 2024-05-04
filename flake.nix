1612e907408c4f22fa3f30aef35114c7b1921c89
Nick Cooke <nj.cooke@outlook.com>
2024-05-04 18:41:47 +0100

    moved home folder

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.nick = {
-                imports = [ ./home.nix ];
+                imports = [ ./home/home.nix ];
 	      };
 	    }
 	  ];
