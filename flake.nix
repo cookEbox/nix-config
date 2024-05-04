1c8634ffde5e79e4c22da57d321095c6c3deedc8
Nick Cooke <nj.cooke@outlook.com>
2024-05-04 15:55:34 +0100

    minor updates

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
-      url = github:nix-community/home-manager/release-23.11;
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,22 +13,22 @@
   outputs = { self, nixpkgs, home-manager }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-	config.allowUnfree = true;
-      };
+      # pkgs = import nixpkgs {
+      #   inherit system;
+      #   config.allowUnfree = true;
+      # };
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
         nixBox = lib.nixosSystem {
-	  inherit system;
-	  modules = [ 
-	    ./configuration.nix 
-	    home-manager.nixosModules.home-manager {
-	      home-manager.useGlobalPkgs = true;
-	      home-manager.useUserPackages = true;
-	      home-manager.users.nick = {
-	        imports = [ ./home.nix ];
+          inherit system;
+          modules = [ 
+            ./configuration.nix 
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nick = {
+                imports = [ ./home.nix ];
 	      };
 	    }
 	  ];
