70e9054da4ad0e82e6832dba8c2351954bf89f61
Nick Cooke <nj.cooke@outlook.com>
2024-05-04 18:42:40 +0100

    changed to deafult.nix

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.nick = {
-                imports = [ ./home/home.nix ];
+                imports = [ ./home ];
 	      };
 	    }
 	  ];
