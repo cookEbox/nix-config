428cffbc75a03fbc69ec4ea8437673732b2350a0
Nick Cooke <nj.cooke@outlook.com>
2025-02-03 21:55:23 +0000

    testing less simple

--- flake.nix
+++ flake.nix
@@ -11,63 +11,63 @@
   outputs = { self, nixpkgs, home-manager, nixpkgs-unstable }:
     let
       system = "x86_64-linux";
-      # lib = nixpkgs.lib;
-      # unstable = import nixpkgs-unstable {inherit system;};
+      lib = nixpkgs.lib;
+      unstable = import nixpkgs-unstable {inherit system;};
     in {
-      # nixosConfigurations = {
-      #   nixBox = lib.nixosSystem {
-      #     inherit system;
-      #     modules = [ 
-      #       ./hosts/desktop
-      #       home-manager.nixosModules.home-manager {
-      #         home-manager.useGlobalPkgs = true;
-      #         home-manager.useUserPackages = true;
-      #         home-manager.extraSpecialArgs = { inherit unstable; };
-      #         home-manager.users.nick = {
-      #           imports = [ ./hosts/desktop/home ];
-      #         };
-      #       }
-      #     ];
-      #   };
-      #   nixLap = lib.nixosSystem {
-      #     inherit system;
-      #     modules = [ 
-      #       ./hosts/laptop
-      #       home-manager.nixosModules.home-manager {
-      #         home-manager.useGlobalPkgs = true;
-      #         home-manager.useUserPackages = true;
-      #         home-manager.extraSpecialArgs = { inherit unstable; };
-      #         home-manager.users.nick = {
-      #           imports = [ ./hosts/laptop/home ];
-      #         };
-      #       }
-      #     ];
-      #   };
-      #   nixVM = lib.nixosSystem {
-      #     inherit system;
-      #     modules = [ 
-      #       ./hosts/vm
-      #       home-manager.nixosModules.home-manager {
-      #         home-manager.useGlobalPkgs = true;
-      #         home-manager.useUserPackages = true;
-      #         home-manager.users.nick = {
-      #           imports = [ ./hosts/vm/home ];
-      #         };
-      #       }
-      #     ];
-      #   };
-      # };
+      nixosConfigurations = {
+        nixBox = lib.nixosSystem {
+          inherit system;
+          modules = [ 
+            ./hosts/desktop
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit unstable; };
+              home-manager.users.nick = {
+                imports = [ ./hosts/desktop/home ];
+              };
+            }
+          ];
+        };
+        nixLap = lib.nixosSystem {
+          inherit system;
+          modules = [ 
+            ./hosts/laptop
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit unstable; };
+              home-manager.users.nick = {
+                imports = [ ./hosts/laptop/home ];
+              };
+            }
+          ];
+        };
+        nixVM = lib.nixosSystem {
+          inherit system;
+          modules = [ 
+            ./hosts/vm
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nick = {
+                imports = [ ./hosts/vm/home ];
+              };
+            }
+          ];
+        };
+      };
       homeConfigurations = {
         work = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.packages.${system};
           inherit system;
           modules = [
-            # ./hosts/work/home
+            ./hosts/work/home
           ];
           # Optionally include these if desired in your work configuration:
           # home-manager.useGlobalPkgs = true;
           # home-manager.useUserPackages = true;
-          # home-manager.extraSpecialArgs = { inherit unstable; };
+          home-manager.extraSpecialArgs = { inherit unstable; };
         };
       };
     };
--- hosts/work/home/default.nix
+++ hosts/work/home/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, nixpkgs, ... }:
+{ config, pkgs, ... }:
 
 {
    imports = [ 
