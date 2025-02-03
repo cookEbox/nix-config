b3aafc8cbe00f7b9ce6cb34bf66ac3176be63465
Nick Cooke <nj.cooke@outlook.com>
2025-02-03 23:35:41 +0000

    adding a home-manager

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
         };
       };
       homeConfigurations = {
-        work = lib.homeManagerConfiguration {
+        work = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
           modules = [
             ./hosts/work/home
