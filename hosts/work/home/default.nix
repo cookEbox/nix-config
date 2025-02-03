083e4178be4d5c0de4f05363b270634051772764
Nick Cooke <nj.cooke@outlook.com>
2025-02-03 22:04:30 +0000

    removing imports to see if that is the problem

--- hosts/work/home/default.nix
+++ hosts/work/home/default.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-   imports = [ 
-                ../../../home/default.nix
-	 ];
+  #  imports = [ 
+  #               ../../../home/default.nix
+	 # ];
    home = { 
      packages = with pkgs; [ 
      ];
