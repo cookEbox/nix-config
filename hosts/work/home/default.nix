0a0f5a0a5afff27478857d8acbcc09d33b4a095c
Nick Cooke <nj.cooke@outlook.com>
2025-02-13 20:37:46 +0000

    added all bits together

--- hosts/work/home/default.nix
+++ hosts/work/home/default.nix
@@ -1,55 +1,53 @@
 {pkgs, ...}: 
-# {
-#     home.username = "nick";
-#     home.homeDirectory = "/home/nick";
-#     # home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
-#     programs.home-manager.enable = true;
-# }
 {
-   imports = [ 
+  home.username = "nick";
+  home.homeDirectory = "/home/nick";
+  # home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
+  programs.home-manager.enable = true;
+  imports = [ 
                 ../../../home/zsh
                 ../../../home/rofi
                 ../../../home/tmux
                 ../../../home/nvim
                 ../../../home/alacritty
                 ../../../home/starship
-	 ];
-   home = { 
-     # stateVersion = "23.05";
-     packages = with pkgs; [ 
-       jq
-       deja-dup
-       ranger
-       redshift
-       gitui
-       tty-clock
-       nix-direnv
-       qutebrowser
-       nix-prefetch-git
-       networkmanagerapplet
-       dconf2nix
-       mate.mate-tweak
-       simple-scan
-       lsd
-       bat
-       btop
-       git
-       git-crypt
-       gnupg
-       zathura
-       unzip
-       xclip
-       ripgrep
-       tldr
-       nerdfonts
-       hydra-check
-       mpv
-       feh
-       lsof
-       xdotool
-       direnv
-       nil
-       lua-language-server
-     ];
-   };
+	];
+  home = { 
+    # stateVersion = "23.05";
+    packages = with pkgs; [ 
+      jq
+      deja-dup
+      ranger
+      redshift
+      gitui
+      tty-clock
+      nix-direnv
+      qutebrowser
+      nix-prefetch-git
+      networkmanagerapplet
+      dconf2nix
+      mate.mate-tweak
+      simple-scan
+      lsd
+      bat
+      btop
+      git
+      git-crypt
+      gnupg
+      zathura
+      unzip
+      xclip
+      ripgrep
+      tldr
+      nerdfonts
+      hydra-check
+      mpv
+      feh
+      lsof
+      xdotool
+      direnv
+      nil
+      lua-language-server
+    ];
+  };
 }
