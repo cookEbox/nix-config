e914afed8fe5793ad926e95844cd4cec528c7708
Nick Cooke <nj.cooke@outlook.com>
2025-02-15 02:48:28 +0000

    updating zsh to make more generalised

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -36,7 +36,7 @@
        };
      history = {
        size = 10000;
-       path = "/home/nick/.config/zsh/history";
+       path = "~/.config/zsh/history";
      };
      plugins = with pkgs; [
        {
