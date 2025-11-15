b91587342975937b1fc52102095fea5b56eaa284
Nick Cooke <nj.cooke@outlook.com>
2025-11-15 15:52:17 +0000

    mac kitty specific - config

--- home/macKitty/default.nix
+++ home/macKitty/default.nix
@@ -6,10 +6,6 @@
     settings = {
       macos_option_as_alt = "both";
       shell.program = "${pkgs.tmux}/bin/tmux";
-      window.padding = {
-        x = 5;
-        y = 0;
-      };
     };
   };
 }
