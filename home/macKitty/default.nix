82f6c719d405efc1569918abe17e72916cdcbd74
Nick Cooke <nj.cooke@outlook.com>
2026-02-04 00:17:10 +0000

    trying overriding at the os level

--- home/macKitty/default.nix
+++ home/macKitty/default.nix
@@ -7,6 +7,13 @@
       macos_option_as_alt = "both";
       shell = "${pkgs.tmux}/bin/tmux";
     };
+
+    # On macOS, Option+<key> can be interpreted as a special character depending on keyboard layout.
+    # Force Option+j/k to send an ESC-prefixed sequence so tmux sees Meta-j/Meta-k.
+    keybindings = {
+      "alt+j" = "send_text all \\x1bj";
+      "alt+k" = "send_text all \\x1bk";
+    };
   };
   home.file = { 
     ".aerospace.toml".source = ./aerospace.toml;
