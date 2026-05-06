18691e09e24d0fcb447c49d9cb635af35813fbbd
Nick Cooke <nj.cooke@outlook.com>
2026-05-06 15:53:21 +0100

    allow unfree on mac

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -120,7 +120,7 @@
       sshpi = "ssh Admin@192.168.1.116";
       wine888     = ''WINEPREFIX=$HOME/.wine-888 WINEARCH=win64 wine'';
       wineequilab = ''WINEPREFIX=$HOME/.wine-equilab WINEARCH=win32 wine'';
-      macup = "nix flake metadata github:cookEbox/nix-config --refresh; nix run github:nix-community/home-manager/release-25.11 -- switch --flake 'github:cookEbox/nix-config#workMac' --impure; source ~/.zshrc";
+      macup = "export NIXPKGS_ALLOW_UNFREE=1; nix flake metadata github:cookEbox/nix-config --refresh; nix run github:nix-community/home-manager/release-25.11 -- switch --flake 'github:cookEbox/nix-config#workMac' --impure; source ~/.zshrc";
       sdkman = ''source "$HOME/.sdkman/bin/sdkman-init.sh"''; 
       };
     history = {
